37946f24baceb77e69bce98b6905b1c4ba4c693a
davejonesbkk <davejonesbkk@gmail.com>
2018-01-11 15:07:17 +0700

    Added Newsapi.org

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template
 
 from flask_ask import Ask, statement, question, session 
 
-import json, requests, time, unidecode 
+import json, requests, time
 
+from unidecode import unidecode
 
+with open('keys.txt', 'r') as fp:
+	credentials = [x.strip() for x in fp.readlines()]
+
+url = ('https://newsapi.org/v2/top-headlines?'
+'sources=bbc-news&' + credentials[0])
+
+response=requests.get(url)
 
 app = Flask(__name__)
 
@@ -16,10 +24,19 @@ def homepage():
 	return 'Hello foo bar baz'
 	
 	
-@app.route('/')
-def get_post_titles():
+@app.route('/news')
+def get_news():
 
-	pass
+	print response.json()
+	
+	return render_template('news.html')
+	
+@ask.intent('HelloIntent')
+def hello(firstname):
+	text = render_template('hello', firstname=firstname)
+	return statement(text).simple_card('Hello', text)
+	
+		
 	
 if __name__ == '__main__':
 	app.run(debug=True)
